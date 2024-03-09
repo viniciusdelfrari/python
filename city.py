d2384deca230a5e4d3fec08674b7e99e70e22316
Vinicius Del Frari <viniciusdelfrari@gmail.com>
2024-03-09 00:08:30 -0300

    Working

---
+++ city.py
@@ -0,0 +1,6 @@
+print("Welcome!") # Print a greeting
+name = input("What's your name? ") # Ask for a name
+city = input("Which city were you born in? ") # Ask the user what city they were born in
+print(f'Hello, {name} ! I have heard that {city} is a great place!') # Print a message by combining strings and information stored in variables
+print(f'I hope to visit {city} one day!') # Print a message by combining strings and information stored in variables 
+print(f'Have a nice day, {name}!') 
