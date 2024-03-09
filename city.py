8a731d18d9ad4ebaf16f8ea8da3cfa6bdc7878b5
Vinicius Del Frari <viniciusdelfrari@gmail.com>
2024-03-09 12:32:43 -0300

    ok

--- city.py
+++ city.py
@@ -1,6 +1,7 @@
 print("Welcome!") # Print a greeting
-name = input("What's your name? ") # Ask for a name
-city = input("Which city were you born in? ") # Ask the user what city they were born in
+name = input("What's your name? ") # I used the variable "name" to store the information entered by the user.
+city = input("Which city were you born in? ") # I used the variable "city" to store the information entered by the user.
+# I used "f-strings", which are a way of putting a variable inside a text.
 print(f'Hello, {name} ! I have heard that {city} is a great place!') # Print a message by combining strings and information stored in variables
 print(f'I hope to visit {city} one day!') # Print a message by combining strings and information stored in variables 
 print(f'Have a nice day, {name}!') 
