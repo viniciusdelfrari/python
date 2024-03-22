42fc3d250f9348f946b29e3fc2a52209262e8797
Vinicius Del Frari <viniciusdelfrari@gmail.com>
2024-03-22 13:53:05 -0300

    ok

---
+++ number_while.py
@@ -0,0 +1,27 @@
+while True:
+    num = int(input("Please enter a whole number (i.e. an integer): "))
+    print(f"The number you entered is {num}.")
+
+    # Check if the number is even or odd
+    if num % 2 == 0:
+        print(f"{num} is an even number.")
+    else:
+        print(f"{num} is an odd number.")
+
+    # Check if the number has a perfect square root
+    if int(num**0.5)**2 == num:
+        print(f"{num} has a perfect square root.")
+    else:
+        print(f"{num} does not have a perfect square root.")
+
+    # Find factors of the number
+    factors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            factors.append(i)
+    print(f"The factors of {num} are {', '.join(map(str, factors))}.")
+
+    choice = input("Would you like to enter another number? (Y/n): ").strip().lower()
+    if choice != 'y':
+        print("Thank you for playing!")
+        break
